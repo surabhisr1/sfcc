d50b49a19baf5489850abf6db260bd570a09c002
surabhisr1 <89798793+surabhisr1@users.noreply.github.com>
2021-10-26 09:47:46 +0530

    Initialize fix-wishlist-bug

---
+++ packages/pwa/tests/lighthouserc.js
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2021, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+module.exports = {
+    // The different Lighthouse CI configuration options:
+    // https://github.com/GoogleChrome/lighthouse-ci/blob/master/docs/configuration.md
+    ci: {
+        collect: {
+            startServerCommand: 'npm run start',
+            //TODO: Add key pages URLs here as they are implemented.
+            url: ['http://localhost:3000/'],
+            startServerReadyPattern: 'HTTP development server listening on',
+            startServerReadyTimeout: 15000
+        },
+        upload: {
+            target: 'temporary-public-storage'
+        },
+        assert: {
+            aggregationMethod: 'median',
+            assertions: {
+                //TODO: Adjust scores and the assertions level from 'warn' to 'error' to fail the build if the scores are not met.
+                'categories:performance': ['warn', {minScore: 0.8}],
+                'categories:pwa': ['warn', {minScore: 0.9}],
+                'categories:seo': ['warn', {minScore: 0.9}],
+                'categories:accessibility': ['error', {minScore: 0.96}]
+            }
+        }
+    }
+}
