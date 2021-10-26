fd74fce55c71698dd184eea5296bd6b1c5b66443
surabhisr1 <89798793+surabhisr1@users.noreply.github.com>
2021-10-26 09:48:20 +0530

    Initialize snyk-fix-c859b12a9fd60ca8d468ad7c32446358

---
+++ packages/progressive-web-sdk/src/components/grid/index.js
@@ -0,0 +1,8 @@
+/* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
+/* Copyright (c) 2021 Mobify Research & Development Inc. All rights reserved. */
+/* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
+
+import Grid from './grid'
+import GridSpan from './grid-span'
+
+export {Grid, GridSpan}
---
+++ packages/progressive-web-sdk/src/integration-manager/custom/commands.js
@@ -0,0 +1,11 @@
+/* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
+/* Copyright (c) 2021 Mobify Research & Development Inc. All rights reserved. */
+/* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
+
+const customCommands = {}
+
+export const register = (commands) => {
+    Object.assign(customCommands, commands)
+}
+
+export {customCommands}
