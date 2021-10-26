aff943d582af5f75a83d109d5e8303f8db37cea3
surabhisr1 <89798793+surabhisr1@users.noreply.github.com>
2021-10-26 09:47:54 +0530

    Initialize snyk-upgrade-25d155ffd2586b3202b71959e5fc6b11

---
+++ packages/progressive-web-sdk/src/store/add-to-homescreen/selectors.js
@@ -0,0 +1,9 @@
+/* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
+/* Copyright (c) 2021 Mobify Research & Development Inc. All rights reserved. */
+/* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
+
+import {createGetSelector} from 'reselect-immutable-helpers'
+
+export const getAddToHomescreen = ({addToHomescreen}) => addToHomescreen
+
+export const getStatus = createGetSelector(getAddToHomescreen, 'status')
