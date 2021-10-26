2fc4633b33208edb19a784a20264342612b44e13
surabhisr1 <89798793+surabhisr1@users.noreply.github.com>
2021-10-26 09:47:38 +0530

    Initialize billing-address-bug-v2

---
+++ packages/pwa/app/components/product-item/index.jsx
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2021, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import React from 'react'
+import PropTypes from 'prop-types'
+import {FormattedMessage} from 'react-intl'
+import {Box, Flex, Stack, Text, Select} from '@chakra-ui/react'
+import CartItemVariant from '../cart-item-variant'
+import CartItemVariantImage from '../cart-item-variant/item-image'
+import CartItemVariantName from '../cart-item-variant/item-name'
+import CartItemVariantAttributes from '../cart-item-variant/item-attributes'
+import CartItemVariantPrice from '../cart-item-variant/item-price'
+import LoadingSpinner from '../loading-spinner'
+import {noop} from '../../utils/utils'
+
+/**
+ * Component representing a product item usually in a list with details about the product - name, variant, pricing, etc.
+ * @param {Object} product Product to be represented in the list item.
+ * @param {node} primaryAction Child component representing the most prominent action to be performed by the user.
+ * @param {node} secondaryActions Child component representing the other actions relevant to the product to be performed by the user.
+ * @param {func} onItemQuantityChange callback function to be invoked whenever item quantity changes.
+ * @param {boolean} showLoading Renders a loading spinner with overlay if set to true.
+ * @returns A JSX element representing product item in a list (eg: wishlist, cart, etc).
+ */
+const ProductItem = ({
+    product,
+    primaryAction,
+    secondaryActions,
+    onItemQuantityChange = noop,
+    showLoading = false
+}) => {
+    return (
+        <Box position="relative" data-testid={`sf-cart-item-${product.productId}`}>
+            <CartItemVariant variant={product}>
+                {showLoading && <LoadingSpinner />}
+                <Stack layerStyle="cardBordered" align="flex-start">
+                    <Flex width="full" alignItems="flex-start" backgroundColor="white">
+                        <CartItemVariantImage width={['88px', '136px']} mr={4} />
+
+                        <Stack spacing={3} flex={1}>
+                            <Stack spacing={1}>
+                                <CartItemVariantName />
+                                <CartItemVariantAttributes />
+                            </Stack>
+
+                            <Flex align="flex-end" justify="space-between">
+                                <Stack spacing={1}>
+                                    <Text fontSize="sm" color="gray.700">
+                                        <FormattedMessage defaultMessage="Quantity:" />
+                                    </Text>
+                                    <Select
+                                        onChange={(e) => onItemQuantityChange(e.target.value)}
+                                        value={product.quantity}
+                                        width="75px"
+                                    >
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                        <option value="6">6</option>
+                                        <option value="7">7</option>
+                                        <option value="8">8</option>
+                                        <option value="9">9</option>
+                                    </Select>
+                                </Stack>
+                                <Stack>
+                                    <CartItemVariantPrice />
+                                    <Box display={['none', 'block', 'block', 'block']}>
+                                        {primaryAction}
+                                    </Box>
+                                </Stack>
+                            </Flex>
+
+                            {secondaryActions}
+                        </Stack>
+                    </Flex>
+                    {!showLoading && (
+                        <Box display={['block', 'none', 'none', 'none']} w={'full'}>
+                            {primaryAction}
+                        </Box>
+                    )}
+                </Stack>
+            </CartItemVariant>
+        </Box>
+    )
+}
+
+ProductItem.propTypes = {
+    product: PropTypes.object,
+    onItemQuantityChange: PropTypes.func,
+    onAddItemToCart: PropTypes.func,
+    showLoading: PropTypes.bool,
+    isWishlistItem: PropTypes.bool,
+    primaryAction: PropTypes.node,
+    secondaryActions: PropTypes.node
+}
+
+export default ProductItem
