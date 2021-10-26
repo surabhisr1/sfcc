a5c0567abbf34c441ad0035e6c6978fd0401b259
surabhisr1 <89798793+surabhisr1@users.noreply.github.com>
2021-10-26 09:47:43 +0530

    Initialize update-publish-options-v1

---
+++ packages/progressive-web-sdk/src/analytics/constants.js
@@ -0,0 +1,10 @@
+/* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
+/* Copyright (c) 2021 Mobify Research & Development Inc. All rights reserved. */
+/* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
+
+export const PLATFORMS = {
+    UPWA: 'UPWA',
+    PWA: 'PWA',
+    NON_PWA: 'nonPWA',
+    PWA_STANDALONE: 'PWA:standalone'
+}
